2b43e52a25238c2a607e557f1697bb97b1da0f7c
Gabriele Nunez <gabrielenunez@protonmail.com>
2024-02-21 12:46:42 -0500

    Adjusted entity prelude and generated files

--- entity/src/lib.rs
+++ entity/src/lib.rs
@@ -1,3 +1,13 @@
-mod prelude;
+pub mod application_global_settings;
+pub mod application_process_logs;
+pub mod application_processes;
+pub mod application_settings;
+pub mod application_user_settings;
+pub mod applications;
 
-pub use prelude::*;
+pub use application_global_settings::Entity as ApplicationGlobalSetting;
+pub use application_process_logs::Entity as ApplicationProcessLog;
+pub use application_processes::Entity as ApplicationProcesse;
+pub use application_settings::Entity as ApplicationSetting;
+pub use application_user_settings::Entity as ApplicationUserSetting;
+pub use applications::Entity as Application;
