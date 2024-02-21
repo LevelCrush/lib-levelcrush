cc7e11812fc3d64aac96fa1c5bb3c377a2a2bd0b
Gabriele Nunez <gabrielenunez@protonmail.com>
2024-02-20 21:04:31 -0500

    setup application table and application settings schema

--- migration/src/m20240220_232237_start.rs
+++ migration/src/m20240220_232237_start.rs
@@ -6,42 +6,74 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
             .create_table(
                 Table::create()
-                    .table(Post::Table)
+                    .table(Application::Table)
                     .if_not_exists()
                     .col(
-                        ColumnDef::new(Post::Id)
+                        ColumnDef::new(Application::Id)
                             .integer()
                             .not_null()
                             .auto_increment()
                             .primary_key(),
                     )
-                    .col(ColumnDef::new(Post::Title).string().not_null())
-                    .col(ColumnDef::new(Post::Text).string().not_null())
+                    .col(ColumnDef::new(Application::Hash).char_len(32).not_null())
+                    .col(ColumnDef::new(Application::HashSecret).char_len(32).not_null())
+                    .col(ColumnDef::new(Application::Name).string_len(32).not_null())
+                    .col(ColumnDef::new(Application::Host).string_len(255).not_null())
+                    .to_owned(),
+            )
+            .await?;
+
+        manager
+            .create_table(
+                Table::create()
+                    .table(ApplicationSettings::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(ApplicationSettings::Id)
+                            .integer()
+                            .not_null()
+                            .auto_increment()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(ApplicationSettings::Hash).char_len(32).not_null())
+                    .col(ColumnDef::new(ApplicationSettings::Name).string_len(255).not_null())
+                    .col(ColumnDef::new(ApplicationSettings::Value).text().not_null().default(""))
                     .to_owned(),
             )
             .await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
+        manager
+            .drop_table(Table::drop().table(Application::Table).to_owned())
+            .await?;
 
         manager
-            .drop_table(Table::drop().table(Post::Table).to_owned())
+            .drop_table(Table::drop().table(ApplicationSettings::Table).to_owned())
             .await
     }
 }
 
 #[derive(DeriveIden)]
-enum Post {
+#[sea_orm(rename = "applications")]
+enum Application {
     Table,
     Id,
-    Title,
-    Text,
+    Hash,
+    HashSecret,
+    Name,
+    Host,
+}
+
+#[derive(DeriveIden)]
+#[sea_orm(rename = "application_settings")]
+enum ApplicationSettings {
+    Table,
+    Id,
+    Hash,
+    Name,
+    Value,
 }
